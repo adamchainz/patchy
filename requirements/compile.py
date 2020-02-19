14d8d4730e8de2098a267c3b6898c6af33a037bd
Thomas Grainger <tagrain@gmail.com>
2020-02-19 09:45:00 +0000

    avoid changing working dir and environ of current env

--- requirements/compile.py
+++ requirements/compile.py
@@ -2,22 +2,19 @@
 import os
 import subprocess
 import sys
+import functools
 from pathlib import Path
 
 
 if __name__ == "__main__":
-    os.chdir(Path(__file__).parent)
-    os.environ["CUSTOM_COMPILE_COMMAND"] = "requirements/compile.py"
     common_args = ["-m", "piptools", "compile", "--generate-hashes"] + sys.argv[1:]
-    subprocess.run(
-        ["python3.5", *common_args, "-o", "py35.txt"], check=True,
+    run = functools.partial(
+        subprocess.run,
+        check=True,
+        cwd=Path(__file__).parent,
+        env={**os.environ, "CUSTOM_COMPILE_COMMAND": "requirements/compile.py"},
     )
-    subprocess.run(
-        ["python3.6", *common_args, "-o", "py36.txt"], check=True,
-    )
-    subprocess.run(
-        ["python3.7", *common_args, "-o", "py37.txt"], check=True,
-    )
-    subprocess.run(
-        ["python3.8", *common_args, "-o", "py38.txt"], check=True,
-    )
+    run(["python3.5", *common_args, "-o", "py35.txt"])
+    run(["python3.6", *common_args, "-o", "py36.txt"])
+    run(["python3.7", *common_args, "-o", "py37.txt"])
+    run(["python3.8", *common_args, "-o", "py38.txt"])
