527b9e1f6d41fccfb04095ec1736cb45ed26e9f1
Adam Johnson <me@adamj.eu>
2021-08-11 13:40:47 +0100

    Add type hints

--- src/patchy/api.py
+++ src/patchy/api.py
@@ -9,6 +9,19 @@ import sys
 from functools import wraps
 from tempfile import mkdtemp
 from textwrap import dedent
+from types import CodeType, TracebackType
+from typing import (
+    Any,
+    Callable,
+    Dict,
+    List,
+    Optional,
+    Tuple,
+    Type,
+    TypeVar,
+    Union,
+    cast,
+)
 from weakref import WeakKeyDictionary
 
 from .cache import PatchingCache
@@ -24,18 +37,22 @@ __all__ = ("patch", "mc_patchface", "unpatch", "replace", "temp_patch")
 # Public API
 
 
-def patch(func, patch_text):
-    return _do_patch(func, patch_text, forwards=True)
+def patch(func: Union[Callable[..., Any], str], patch_text: str) -> None:
+    _do_patch(func, patch_text, forwards=True)
 
 
 mc_patchface = patch
 
 
-def unpatch(func, patch_text):
-    return _do_patch(func, patch_text, forwards=False)
+def unpatch(func: Union[Callable[..., Any], str], patch_text: str) -> None:
+    _do_patch(func, patch_text, forwards=False)
 
 
-def replace(func, expected_source, new_source):
+def replace(
+    func: Callable[..., Any],
+    expected_source: Optional[str],
+    new_source: str,
+) -> None:
     if expected_source is not None:
         expected_source = dedent(expected_source)
         current_source = _get_source(func)
@@ -45,32 +62,44 @@ def replace(func, expected_source, new_source):
     _set_source(func, new_source)
 
 
+AnyFunc = TypeVar("AnyFunc", bound=Callable[..., Any])
+
+
 class temp_patch:
-    def __init__(self, func, patch_text):
+    def __init__(self, func: Union[Callable[..., Any], str], patch_text: str) -> None:
         self.func = func
         self.patch_text = patch_text
 
-    def __enter__(self):
+    def __enter__(self) -> None:
         patch(self.func, self.patch_text)
 
-    def __exit__(self, _, __, ___):
+    def __exit__(
+        self,
+        exc_type: Union[Type[BaseException], None],
+        exc_val: Union[BaseException, None],
+        exc_tb: Union[TracebackType, None],
+    ) -> None:
         unpatch(self.func, self.patch_text)
 
-    def __call__(self, decorable):
+    def __call__(self, decorable: AnyFunc) -> AnyFunc:
         @wraps(decorable)
-        def wrapper(*args, **kwargs):
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
             with self:
                 decorable(*args, **kwargs)
 
-        return wrapper
+        return cast(AnyFunc, wrapper)
 
 
 # Gritty internals
 
 
-def _do_patch(func, patch_text, forwards):
+def _do_patch(
+    func: Union[Callable[..., Any], str],
+    patch_text: str,
+    forwards: bool,
+) -> None:
     if isinstance(func, str):
-        func = pkgutil_resolve_name(func)
+        func = cast(Callable[..., Any], pkgutil_resolve_name(func))
     source = _get_source(func)
     patch_text = dedent(patch_text)
 
@@ -82,7 +111,12 @@ def _do_patch(func, patch_text, forwards):
 _patching_cache = PatchingCache(maxsize=100)
 
 
-def _apply_patch(source, patch_text, forwards, name):
+def _apply_patch(
+    source: str,
+    patch_text: str,
+    forwards: bool,
+    name: str,
+) -> str:
     # Cached ?
     try:
         return _patching_cache.retrieve(source, patch_text, forwards)
@@ -134,7 +168,7 @@ def _apply_patch(source, patch_text, forwards, name):
     return new_source
 
 
-def _get_flags_mask():
+def _get_flags_mask() -> int:
     result = 0
     for name in __future__.all_feature_names:
         result |= getattr(__future__, name).compiler_flag
@@ -145,10 +179,14 @@ FEATURE_MASK = _get_flags_mask()
 
 
 # Stores the source of functions that have had their source changed
-_source_map = WeakKeyDictionary()
+# Bad type hints because WeakKeyDictionary only indexable on Python 3.9+
+_source_map: Dict[Callable[..., Any], str] = cast(
+    Dict[Callable[..., Any], str],
+    WeakKeyDictionary(),
+)
 
 
-def _get_source(func):
+def _get_source(func: Callable[..., Any]) -> str:
     real_func = _get_real_func(func)
     try:
         return _source_map[real_func]
@@ -158,7 +196,7 @@ def _get_source(func):
         return source
 
 
-def _class_name(func):
+def _class_name(func: Callable[..., Any]) -> Optional[str]:
     split_name = func.__qualname__.split(".")
     try:
         class_name = split_name[-2]
@@ -170,7 +208,7 @@ def _class_name(func):
         return class_name
 
 
-def _set_source(func, func_source):
+def _set_source(func: Callable[..., Any], func_source: str) -> None:
     # Fetch the actual function we are changing
     real_func = _get_real_func(func)
     # Figure out any future headers that may be required
@@ -178,15 +216,20 @@ def _set_source(func, func_source):
 
     class_name = _class_name(func)
 
-    def _compile(code, flags=0):
+    def _compile(
+        code: Union[str, ast.Module],
+        flags: int = 0,
+    ) -> Union[CodeType, ast.Module]:
         return compile(
             code, "<patchy>", "exec", flags=feature_flags | flags, dont_inherit=True
         )
 
-    def _parse(code):
-        return _compile(code, flags=ast.PyCF_ONLY_AST)
+    def _parse(code: str) -> ast.Module:
+        result = _compile(code, flags=ast.PyCF_ONLY_AST)
+        assert isinstance(result, ast.Module)
+        return result
 
-    def _process_freevars():
+    def _process_freevars() -> Tuple[str, ast.AST, List[str]]:
         """
         Wrap the new function in a __patchy_freevars__ method that provides all
         freevars of the original function.
@@ -213,14 +256,14 @@ def _set_source(func, func_source):
         fv_force_use_body = [f"    {fv}" for fv in fvs]
         if fv_force_use_body:
             fv_force_use_ast = _parse("\n".join([_def] + fv_force_use_body))
-            fv_force_use = fv_force_use_ast.body[0].body
+            fv_force_use = fv_force_use_ast.body[0].body  # type: ignore [attr-defined]
         else:
             fv_force_use = []
         _ast = _parse(func_source).body[0]
-        _ast.body = _ast.body + fv_force_use
+        _ast.body = _ast.body + fv_force_use  # type: ignore [attr-defined]
         return _def, _ast, fv_body
 
-    def _process_method():
+    def _process_method() -> ast.Module:
         """
         Wrap the new method in a class to ensure the same mangling as would
         have been performed on the original method:
@@ -248,17 +291,17 @@ def _set_source(func, func_source):
         )
         to_parse = "\n".join([_def] + fv_body + [class_src, ret])
         new_source = _parse(to_parse)
-        new_source.body[0].body[-2].body[0] = _ast
+        new_source.body[0].body[-2].body[0] = _ast  # type: ignore [attr-defined]
         return new_source
 
-    def _process_function():
+    def _process_function() -> ast.Module:
         _def, _ast, fv_body = _process_freevars()
         name = func.__name__
         ret = f"    return {name}"
         _global = [] if name in func.__code__.co_freevars else [f"    global {name}"]
         to_parse = "\n".join([_def] + _global + fv_body + ["    pass", ret])
         new_source = _parse(to_parse)
-        new_source.body[0].body[-2] = _ast
+        new_source.body[0].body[-2] = _ast  # type: ignore [attr-defined]
         return new_source
 
     if class_name:
@@ -267,10 +310,14 @@ def _set_source(func, func_source):
         new_source = _process_function()
 
     # Compile and retrieve the new Code object
-    localz = {}
-    new_code = _compile(new_source)
+    localz: Dict[str, Any] = {}
+    new_code = cast(CodeType, _compile(new_source))
 
-    exec(new_code, dict(func.__globals__), localz)
+    exec(
+        new_code,
+        dict(func.__globals__),  # type: ignore [attr-defined]
+        localz,
+    )
     new_func = localz["__patchy_freevars__"]()
 
     # Put the new Code object in place
@@ -280,19 +327,19 @@ def _set_source(func, func_source):
     _source_map[real_func] = func_source
 
 
-def _get_real_func(func):
+def _get_real_func(func: Callable[..., Any]) -> Callable[..., Any]:
     """
     Duplicates some of the logic implicit in inspect.getsource(). Basically
     some function-esque things, such as classmethods, aren't functions but we
     can peel back the layers to the underlying function very easily.
     """
     if inspect.ismethod(func):
-        return func.__func__
+        return func.__func__  # type: ignore [attr-defined]
     else:
         return func
 
 
-def _assert_ast_equal(current_source, expected_source, name):
+def _assert_ast_equal(current_source: str, expected_source: str, name: str) -> None:
     current_ast = ast.parse(current_source)
     expected_ast = ast.parse(expected_source)
     if not ast.dump(current_ast) == ast.dump(expected_ast):
